90733e509c111ab218fabc92e1fbfb35002bdc64
seonghwa.kim <sh827kim@gmail.com>
2022-06-12 16:55:12 +0900

    rxjava 3 chapter added

---
+++ src/main/java/ch06/ColdPublisherExample.java
@@ -0,0 +1,12 @@
+package ch06;
+
+import io.reactivex.rxjava3.core.Flowable;
+
+public class ColdPublisherExample {
+    public static void main(String[] args) {
+        var flowable = Flowable.just(1, 2, 3, 4, 5);
+
+        flowable.subscribe(data -> System.out.println("Subscriber 1: " + data));
+        flowable.subscribe(data -> System.out.println("Subscriber 2: " + data));
+    }
+}
---
+++ src/main/java/ch06/HotPublisherExample.java
@@ -0,0 +1,17 @@
+package ch06;
+
+import io.reactivex.rxjava3.processors.PublishProcessor;
+
+public class HotPublisherExample {
+    public static void main(String[] args) {
+        var processor = PublishProcessor.create();
+        processor.subscribe(data -> System.out.println("Subscriber 1: " + data));
+        processor.onNext(1);
+        processor.onNext(2);
+
+        processor.subscribe(data -> System.out.println("Subscriber 2: " + data));
+        processor.onNext(3);
+        processor.onNext(4);
+
+    }
+}
